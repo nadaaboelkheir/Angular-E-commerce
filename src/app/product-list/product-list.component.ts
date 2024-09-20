796983d7a51cbacdc30577a7829b602780cd935d
nada <nadaaboelkheir1@gmail.com>
2024-09-20 04:39:19 +0300

    Fix : search

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,44 +3,36 @@ import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../types/product';
 import { ProductService } from '../requests/product.service';
-
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [CommonModule, FormsModule, ProductCardComponent],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products: Array<Product> = [];
   filteredProducts: Array<Product> = [];
   searchTerm: string = '';
-  searchPerformed: boolean = false;
-constructor(private productService: ProductService) {}
+
+  constructor(private productService: ProductService) {}
+
   ngOnInit(): void {
-   this.productService.getProductList().subscribe((data) => {
-     this.products = data.products
-   })
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data.products;
+      this.filteredProducts = data.products; 
+    });
   }
 
-  // Method to handle the search when the search button is clicked
-  performSearch(): void {
-    if (this.searchTerm) {
+  searchProducts(): void {
+    if (this.searchTerm.trim() === '') {
+      this.filteredProducts = this.products; 
+    } else {
       this.filteredProducts = this.products.filter((product) =>
         product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
-    } else {
-      this.filteredProducts = this.products;
     }
-    this.searchPerformed = true;
   }
-
-  resetSearch(): void {
-    this.searchTerm = '';
-    this.filteredProducts = this.products;
-    this.searchPerformed = false;
-  }
-
-  
 }
